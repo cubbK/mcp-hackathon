b7d05031688bc467c46d8dc40e2900d07d5b5a8c
Dan <yo@gmail.om>
2025-08-13 15:01:45 +0200

    start fastmcp server

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from mcp-hackathon!")
+from fastmcp import FastMCP
+
+# Create a server instance
+mcp = FastMCP(name="GithubOrgExplorer")
+
+
+@mcp.tool
+def info() -> str:
+    """Provides information about the tool."""
+    return "This is a tool for exploring GitHub organizations."
 
 
 if __name__ == "__main__":
-    main()
+    mcp.run()
